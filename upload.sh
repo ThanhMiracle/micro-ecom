1354532bd0a6b0f0ab54ecf27c051f514cb3797b
thanh <dtthanh2909@gmail.com>
2026-02-26 09:40:15 +0700

    update linh tinh

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-services=("web" "auth" "product" "order" "notify")
+services=("web" "auth" "product" "order" "notify" "payment")
 
 for s in "${services[@]}"; do
-  docker tag microshop_fullstack-$s:latest thanh2909/$s-service:v0.1
-  docker push thanh2909/$s-service:v0.1
+  docker tag microshop_fullstack-$s:latest thanh2909/$s-service:v0.3
+  docker push thanh2909/$s-service:v0.3
 done
